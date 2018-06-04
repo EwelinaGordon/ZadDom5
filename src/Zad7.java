ac52185adc5ca0acf217e45a8ddd17f496ae29e0
EwelinaGordon <ewelina.gordon@gmail.com>
2018-06-04 22:33:19 +0200

    Poprawa: zaczytywanie do tablicy

--- src/Zad7.java
+++ src/Zad7.java
@@ -1,17 +1,14 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Zad7 {
     public static void main(String[] args) {
         String[] nameTable = new String[5];
-
         Scanner scanner = new Scanner(System.in);
         System.out.println("Podaj 5 imion: ");
-        nameTable[0] = scanner.nextLine();
-        nameTable[1] = scanner.nextLine();
-        nameTable[2] = scanner.nextLine();
-        nameTable[3] = scanner.nextLine();
-        nameTable[4] = scanner.nextLine();
+
+        for (int i = 0; i <5; i++) {
+            nameTable[i] = scanner.nextLine();
+        }
 
         System.out.println(" ");
         for (int i = 4; i >= 0; i--) {
