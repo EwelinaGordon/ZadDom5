640a0ab9e148b8fc576e80c8eca6273353f95a59
EwelinaGordon <ewelina.gordon@gmail.com>
2018-06-04 22:24:39 +0200

    Poprawa: jedna pętla

--- src/Zad6.java
+++ src/Zad6.java
@@ -8,29 +8,18 @@ public class Zad6 {
 
         int x = scanner.nextInt();
 
-        while (x<100 || x> 200){
-            if(x<100){
-           System.out.println("Podana liczba jest za mała");
-                System.out.println("Podaj liczbę: ");
-                x = scanner.nextInt();
-                continue;
 
-          } else if (x > 200) {
-           System.out.println("Podana liczba jest za duża");
+        while (x < 100 || x > 200 || x % 3 != 0) {
+            if (x < 100) {
+                System.out.println("Podana liczba jest za mała");
+            } else if (x > 200) {
+                System.out.println("Podana liczba jest za duża");
+            } else {
+                System.out.println("Liczba nie jest podzielna przez trzy");
+            }
             System.out.println("Podaj liczbę: ");
             x = scanner.nextInt();
-            continue;}
-
         }
-
-        while (x%3!=0){
-            System.out.println("Liczba nie jest podzielna przez 3");
-            System.out.println("Podaj liczbę: ");
-            x = scanner.nextInt();
-            continue;}
-
-
         System.out.println("Jest OK");
-
- }
+    }
 }
